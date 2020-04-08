f84c3422b77da3a7b68fd9b0ceaa15404f29fa61
Pavel Batsev <PBatsev@sportmaster.ru>
2020-04-09 00:45:47 +0300

    immon4ik. ansible-role-db. Edit files for task.

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -12,14 +12,8 @@ def test_mongo_running_and_enabled(host):
     assert mongo.is_running
     assert mongo.is_enabled
 
-
 # check if configuration file contains the required line
-def test_config_file(host):
-    config_file = host.file('/etc/mongod.conf')
+def test_config_file(File):
+    config_file = File('/etc/mongod.conf')
     assert config_file.contains('bindIp: 0.0.0.0')
     assert config_file.is_file
-
-
-def test_mongo_port_is_open(host):
-    port = host.socket("tcp://0.0.0.0:27017")
-    assert port.is_listening
