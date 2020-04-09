d8b3ad32a1556c8dc467d14af531746917fccdbf
Pavel Batsev <PBatsev@sportmaster.ru>
2020-04-09 08:00:47 +0300

    immon4ik. ansible-role-db. Fix test_default.py.

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -3,7 +3,7 @@ import os
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('localhost')
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
 # check if MongoDB is enabled and running
@@ -12,8 +12,14 @@ def test_mongo_running_and_enabled(host):
     assert mongo.is_running
     assert mongo.is_enabled
 
+
 # check if configuration file contains the required line
-def test_config_file(File):
-    config_file = File('/etc/mongod.conf')
+def test_config_file(host):
+    config_file = host.file('/etc/mongod.conf')
     assert config_file.contains('bindIp: 0.0.0.0')
     assert config_file.is_file
+
+
+def test_mongo_port_is_open(host):
+    port = host.socket("tcp://0.0.0.0:27017")
+    assert port.is_listening
